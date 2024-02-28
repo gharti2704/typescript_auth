ef2a6deea91bf0580858e086b99f70de1ff14993
Raju Gharti <gharti.rg@gmail.com>
2024-02-28 01:04:36 -0500

    seperate routes

--- build/app.js
+++ build/app.js
@@ -1,13 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import router from './routes/auth.js';
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/api/auth', router);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on htt://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { Request, Response, Express } from 'express';
+import express, { Express } from 'express';
+import router from './routes/auth.js';
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response): void => {
-  res.send('Hello World');
-});
+app.use('/api/auth', router);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running on htt://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
